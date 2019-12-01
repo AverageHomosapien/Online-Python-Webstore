69553f2cc944d37fd02a893c939579773593a128
Calum Hamilton <hamcalum10@gmail.com>
2019-12-01 15:06:40 +0000

    Site Update

--- app.py
+++ app.py
@@ -1,26 +1,41 @@
 from flask import Flask, render_template
 import random
-from flask_login import current_user
+import flask_login
+from flask_login import current_user, LoginManager
+from config import Config
 
 app = Flask(__name__, static_folder="static")
-user_auth = False
+app.config.from_object('config')
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-@app.route('/')
-@app.route('/index')
-def names():
-    #names = ["Harry", "Tommy", "Johnny", "Franz Winklebottom"]
-    #return render_template('index_for_user.html', names=names)
-    if current_user.is_authenticated:
-         return render_template("index_for_user.html")
-    else:
-         return render_template("index_for_anomymous.html")
+#@app.route('/hello/<user>')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+def names():        #return render_template('index_for_user.html', names=names)
+    #facultyId = name if facultyId else None
+    #if current_user.is_authenticated:
+    #     return render_template("index.html")
+    #else:
+    #     return render_template("register.html")
+    return render_template("index.html")
 
-
-@app.route('/register')
-@app.route('/login')
+@app.route('/register', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('register.html')
+    if current_user.is_authenticated:
+        return render_template("index_for_user.html")
+    else:
+        return render_template("register.html")
 
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    #logout the user
+    render_template("register.html")
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 if __name__ == '__main__':
         app.run(debug=True)
